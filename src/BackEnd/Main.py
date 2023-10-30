f50318147801929f39bd352cb9b4f3c47dcc7001
bryan967132 <bryan.tejaxun@gmail.com>
2023-10-29 19:14:47 -0600

    Main

--- src/BackEnd/Main.py
+++ src/BackEnd/Main.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 from Language.Parser import *
+from Env.Env import *
 import os
 
 app = Flask(__name__)
@@ -19,6 +20,12 @@ def parse():
         return jsonify({'response': f'{response["commentary"]}'})
     except:
         return jsonify({'response': f' -> Error: Comando sin reconocer. [{request.json["line"]}:1]'})
+
+@app.route('/isLogged', methods=['GET'])
+def isLogged():
+    if currentLogged['User']:
+        return jsonify({'isLogged': True})
+    return jsonify({'isLogged': False})
 
 if __name__ == '__main__':
     os.system('clear')
