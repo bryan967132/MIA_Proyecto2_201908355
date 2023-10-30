4886af39fee813d44289db5c6e69cad5ee2becf3
bryan967132 <bryan.tejaxun@gmail.com>
2023-10-29 19:14:15 -0600

    Card

--- src/FrontEnd/src/components/Card.js
+++ src/FrontEnd/src/components/Card.js
@@ -1,5 +1,5 @@
 import React, { useState, useRef } from 'react';
-import './card.css'
+import './Card.css'
 import { headers, API } from './headers.js'
 import CodeMirror from '@uiw/react-codemirror';
 import { StreamLanguage } from '@codemirror/language';
